621e97b92c1dd4ed84bee9fa80a9e0a972183d34
Ahad Wasim <ahadwasim@gmail.com>
2015-04-23 18:42:57 -0700

    Still testing

---
+++ form_validation.php
@@ -0,0 +1,55 @@
+<?php
+
+	$connection = mysqli_connect('ahadwasim.com','root', 'bengals1','portfolio');
+	$error=[];
+	$output=[];
+
+	if(isset($_POST))
+	{
+		if(empty($_POST['name']))
+		{
+			$error['name']='Please enter in a name';
+		}
+		if(empty($_POST['email']))
+		{
+			$error['email']='Please enter in a email';
+		} 
+		if(empty($_POST['message']))
+		{
+			$error['message']='Really your sending a blank message';
+		}
+		
+
+		if(count($error) == 0){
+				
+				$t=time();
+				$t=(date("m-d-Y",$t));
+				$fullname = $_POST['name'];
+				$email = $_POST['email'];
+				$message = $_POST['message'];
+
+			$query = " INSERT INTO email (`name`,`email`,`message`,`time`) VALUES ('$fullname','$email', '$message','$t')" ;
+
+
+			$result = mysqli_query($connection, $query);
+
+			$output['success'] = true; // this will help us pass the conditional on the ajax call
+			$output['message'] = 'Message Sent';
+
+		} else {
+			$output['success']=false;
+        	$output['message']= $error;
+		}
+	} else {
+			$output['success'] = false;
+			$output['message'] = "Message Failed to Send";
+	}
+
+	//require('assets/send.php');
+	
+	echo json_encode($output);
+
+?>
+
+
+
