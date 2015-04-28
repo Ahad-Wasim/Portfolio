b1e95adea029a6b549f5079f5a609ab83319a3d7
Ahad Wasim <ahadwasim@gmail.com>
2015-04-28 14:50:17 -0700

    Fixed some wording

--- index.php
+++ index.php
@@ -242,8 +242,8 @@
 						<h3>Tipify</h3>
 						<p>
 							An application for tipping restaurant employees and writing reviews about them based on their service provided.
-							Tipify also features a tipping calculator which helps the consumer identify the appropriate number each employee should be tipped. Tipify also allows the consumer
-							to tip their preferred employee on the spot.
+							Tipify features a tipping calculator which helps the consumer identify the appropriate number each employee should be tipped and allows you 
+							to tip them on the spot. 
 							<h3 class = 'url'><strong>Note: The app is still a work in process  </strong>URL: <a style ='text-decoration: none' href = 'https://github.com/Ahad-Wasim/Tipify' target='_blank'>Tipify</a></h3>
 
 							
